694c9ae1b0c849a9df26ca9827abdbc51cf55a7c
richie.lee <richie@bzzzt.io>
2021-08-12 11:20:34 +0100

    end of chapter 4

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -2,6 +2,14 @@ terraform {
   required_version = ">=1.0.3"
 }
 
+locals {
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
+}
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -14,24 +22,24 @@ data "terraform_remote_state" "db" {
   backend = "s3"
 
   config = {
-    bucket = "terraform-up-and-running-state-richiebzzzt"
-    key    = "stage/datastores/mysql/terraform.state"
+    bucket = var.db_remote_state_bucket
+    key    = var.db_remote_state_key
     region = "us-east-2"
   }
 }
 
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0c55b159cbfafe1f0"
-  instance_type   = "t2.micro"
+  instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
-              
+
   lifecycle {
     create_before_destroy = true
   }
 }
 
 data "template_file" "user_data" {
-  template = file("user-data.sh")
+  template = file("${path.module}/user-data.sh")
 
   vars = {
     server_port = var.server_port
@@ -42,7 +50,7 @@ data "template_file" "user_data" {
 
 resource "aws_security_group" "instance" {
 
-  name = "aws-example-instance"
+  name = "${var.cluster_name}-instance"
 
   ingress {
     from_port   = var.server_port
@@ -53,7 +61,7 @@ resource "aws_security_group" "instance" {
 }
 
 resource "aws_lb" "example" {
-  name               = "terraform-asg-example"
+  name               = "${var.cluster_name}-example"
   load_balancer_type = "application"
   subnets            = data.aws_subnet_ids.default.ids
   security_groups    = [aws_security_group.alb.id]
@@ -61,7 +69,7 @@ resource "aws_lb" "example" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
+  port              = local.http_port
   protocol          = "HTTP"
 
   default_action {
@@ -76,20 +84,24 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
+  name = "${var.cluster_name}-alb"
+}
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+  from_port         = local.http_port
+  to_port           = local.http_port
+  protocol          = local.tcp_protocol
+  cidr_blocks       = local.all_ips
+}
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_lb_target_group" "asg" {
@@ -116,29 +128,28 @@ resource "aws_autoscaling_group" "example" {
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
-  min_size = 2
-  max_size = 10
+  min_size = var.min_size
+  max_size = var.max_size
 
   tag {
     key                 = "Name"
-    value               = "terraform-asg-example"
+    value               = "${var.cluster_name}-example"
     propagate_at_launch = true
   }
 }
 
 resource "aws_lb_listener_rule" "asg" {
   listener_arn = aws_lb_listener.http.arn
-  priority = 100
+  priority     = 100
 
   condition {
-      path_pattern {
-          values = ["*"]
-      }
+    path_pattern {
+      values = ["*"]
+    }
   }
 
   action {
-      type = "forward"
-      target_group_arn = aws_lb_target_group.asg.arn
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
-
--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -4,6 +4,13 @@ provider "aws" {
 
 module "webserver_cluster" {
     source = "../../../modules/services/webserver-cluster"
+    cluster_name = "webservers-stage"
+    db_remote_state_bucket = "terraform-up-and-running-state-richiebzzzt"
+    db_remote_state_key = "stage/datastores/mysql/terraform.state"
+
+    instance_type = "t2.micro"
+    min_size = 2
+    max_size = 2
 }
 
 terraform {
@@ -16,3 +23,22 @@ terraform {
     encrypt        = true
   }
 }
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  recurrence = "0 9 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+  scheduled_action_name = "scale_out_during_business_hours"
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  recurrence = "0 17 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+  scheduled_action_name = "scale_in_at_night"
+}
+
